97f235635cba75b03ea56503c2cb4a66f6a31b70
Karanvir <karanvirboparai123@gmail.com>
2022-12-15 10:05:07 -0500

    created a generic field type

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,18 @@
 import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
 import { AuthorResolver } from "./authors/author.resolvers";
 
+export type fields = {
+  [key: string]: {
+    type: any;
+    args: {
+      [key: string]: {
+        type: any;
+      };
+    };
+    resolve: (parent: any, args: any) => void;
+  };
+};
+
 const RootQuery = new GraphQLObjectType({
   name: "Query",
   fields: {
