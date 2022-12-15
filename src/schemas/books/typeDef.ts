29083508ed57f7123c6ee3520a2474f38844d42f
Karanvir <karanvirboparai123@gmail.com>
2022-12-15 09:36:40 -0500

    created book type

---
+++ src/schemas/books/typeDef.ts
@@ -0,0 +1,44 @@
+import {
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
+import { AuthorType } from "../authors/typedef";
+import { LibraryType } from "../libraries/typeDef";
+import { PublisherType } from "../publishers/typeDef";
+
+export const formatType = new GraphQLObjectType({
+  name: "format",
+  fields: () => ({
+    name: { type: GraphQLString },
+    price: { type: GraphQLInt },
+    url: { type: GraphQLString },
+  }),
+});
+
+export const BookType = new GraphQLObjectType({
+  name: "book",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    asin: { type: GraphQLString },
+    ISBN10: { type: GraphQLString },
+    author: { type: new GraphQLList(AuthorType) },
+    currency: { type: GraphQLString },
+    description: { type: GraphQLString },
+    final_price: { type: GraphQLFloat },
+    format: { type: new GraphQLList(formatType) },
+    image_url: { type: GraphQLString },
+    images_count: { type: GraphQLInt },
+    item_weight: { type: GraphQLString },
+    product_dimensions: { type: GraphQLString },
+    rating: { type: GraphQLString },
+    reviews_count: { type: GraphQLInt },
+    publisher: { type: new GraphQLList(PublisherType) },
+    title: { type: GraphQLString },
+    url: { type: GraphQLString },
+    categories: { type: new GraphQLList(GraphQLString) },
+    libraries: { type: new GraphQLList(LibraryType) },
+  }),
+});
