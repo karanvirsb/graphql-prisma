f70cc4a72a4552e01473a9ee0772961964ca0b50
Karanvir <karanvirboparai123@gmail.com>
2022-12-15 08:35:24 -0500

    created index for prisma

---
+++ prisma/index.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+export default prisma;
+
+async function main() {
+  // const length = books.length;
+  // for (let i = 0; i < length; i++) {
+  //   const book = books[i];
+  //   const addBook = await prisma.book.create({
+  //     data: {
+  //       asin: book.asin,
+  //       currency: book.currency,
+  //       description: book.description,
+  //       format: book.format,
+  //       image_url: book.image_url,
+  //       images_count: book.images_count,
+  //       ISBN10: book.ISBN10,
+  //       item_weight: book.item_weight,
+  //       product_dimensions: book.product_dimensions,
+  //       rating: book.rating,
+  //       reviews_count: book.reviews_count,
+  //       title: book.title,
+  //       url: book.url,
+  //       final_price: book.final_price,
+  //       categories: book.categories,
+  //       author: {
+  //         connect: {
+  //           id: book.author_id,
+  //         },
+  //       },
+  //       libraries: {
+  //         connect: {
+  //           id: book.library_id,
+  //         },
+  //       },
+  //       publisher: {
+  //         connect: {
+  //           id: book.publisher_id,
+  //         },
+  //       },
+  //     },
+  //   });
+  // }
+}
+
+main()
+  .catch((e) => {
+    console.error(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
