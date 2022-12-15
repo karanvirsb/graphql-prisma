30ca82cec030e268c8f2c723378e66ab565a20fd
Karanvir <karanvirboparai123@gmail.com>
2022-12-15 10:34:37 -0500

    added limit and cursor

--- src/schemas/authors/author.resolvers.ts
+++ src/schemas/authors/author.resolvers.ts
@@ -1,4 +1,4 @@
-import { GraphQLList } from "graphql";
+import { GraphQLInt, GraphQLList, GraphQLString } from "graphql";
 import { fields } from "..";
 import prisma from "../../../prisma";
 import { AuthorType } from "./typedef";
@@ -6,9 +6,15 @@ import { AuthorType } from "./typedef";
 export const AuthorResolver: fields = {
   getAllAuthors: {
     type: new GraphQLList(AuthorType),
-    args: {},
-    resolve(parent: any, args: any) {
-      return prisma.author.findMany();
+    args: { limit: { type: GraphQLInt }, cursor: { type: GraphQLInt } },
+    async resolve(parent: any, args: any) {
+      const { limit, cursor } = args;
+      return await prisma.author.findMany({
+        take: limit,
+        skip: 1, // skip the cursor
+        cursor: { id: cursor },
+        orderBy: { id: "asc" },
+      });
     },
   },
 };
