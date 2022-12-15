e322673402b8c7394b6441a87485f7abbc6957e6
Karanvir <karanvirboparai123@gmail.com>
2022-12-15 11:07:20 -0500

    added query client and hydration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Hydrate, QueryClientProvider } from "react-query";
+import { queryClient } from "../src/api";
 
+// initalizing our query client and hydrating the state
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
