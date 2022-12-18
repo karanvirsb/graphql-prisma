003c9a32dcd24598057f5b5ee1d45399971138c0
Karanvir <karanvirboparai123@gmail.com>
2022-12-18 10:02:27 -0500

    created a book resolver with get books

---
+++ src/schemas/books/book.resolvers.ts
@@ -0,0 +1,20 @@
+import { GraphQLInt, GraphQLList } from "graphql";
+import { fields } from "..";
+import prisma from "../../../prisma";
+import { BookType } from "./typeDef";
+
+const BookResolver: fields = {
+  getBooks: {
+    type: new GraphQLList(BookType),
+    args: { limit: { type: GraphQLInt }, page: { type: GraphQLInt } },
+    async resolve(parent: any, args: any) {
+      const { limit, page } = args;
+      // skip: 0 * 10 = 0, 1 * 10 = 10
+      return await prisma.book.findMany({
+        skip: page * limit,
+        take: limit,
+        orderBy: { title: "asc" },
+      });
+    },
+  },
+};
