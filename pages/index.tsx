9db04a8b39c31da2c4e3bede22119f78ee35da8a
Karanvir <karanvirboparai123@gmail.com>
2022-12-18 09:51:02 -0500

    added server side rendering of data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
+import { dehydrate, useQuery } from "react-query";
+
+import { queryClient, getAllAuthorsQuery } from "../src/api";
+
+export async function getServerSideProps() {
+  await queryClient.prefetchQuery(["authors"], () =>
+    getAllAuthorsQuery({ cursor: 1, limit: 10 })
+  );
+
+  // the dehydrated state is matched in hte page props
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
+
 export default function Home() {
-  return <div>hi</div>;
+  const { data } = useQuery(["authors"], () =>
+    getAllAuthorsQuery({ cursor: 1, limit: 10 })
+  );
+
+  return <div>{JSON.stringify(data)}</div>;
 }
