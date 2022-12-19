eeae45aa33ab91bcb976b58e5ab2a9863b934b9b
Karanvir <karanvirboparai123@gmail.com>
2022-12-19 09:25:02 -0500

    created type for paginated books

--- src/entity/book/use_cases/index.ts
+++ src/entity/book/use_cases/index.ts
@@ -1,11 +1,14 @@
 import BookDb from "../db";
 import makePaginatedBooks from "./getPaginatedBooks";
-// types
+// types from db
 export type getBooks = typeof BookDb.getBooks;
 
 const getPaginatedBooks = makePaginatedBooks({
   getPaginatedBooks: BookDb.getBooks,
 });
+
+// types for use cases
+export type getPaginatedBooksType = typeof getPaginatedBooks;
 
 const bookServices = Object.freeze({ getPaginatedBooks });
 
