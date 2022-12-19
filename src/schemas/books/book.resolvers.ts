5f2e9963c13ddbb3e9c1e6fa16801b7e822fdacc
Karanvir <karanvirboparai123@gmail.com>
2022-12-19 10:13:28 -0500

    added new resolver import

--- src/schemas/books/book.resolvers.ts
+++ src/schemas/books/book.resolvers.ts
@@ -1,34 +1,23 @@
-import { GraphQLInt, GraphQLList, GraphQLString } from "graphql";
+import { GraphQLInt } from "graphql";
 import { fields } from "..";
 import prisma from "../../../prisma";
 import { BookType } from "./typeDef";
-import { getPaginatedBooks } from "../../entity/book/use_cases";
+import {
+  getPaginatedBooks,
+  getBooksByTitleUseCase,
+} from "../../entity/book/use_cases";
 import { getBooksResolver } from "./getBooks.resolver";
+import { getBooksByTitleResolver } from "./getBooksByTitle.resolver";
 
 // creating the resolvers
 const getBooks = getBooksResolver({ getPaginatedBooks });
+const getBooksByTitle = getBooksByTitleResolver({
+  getBooksByTitle: getBooksByTitleUseCase,
+});
 
 export const BookResolver: fields = {
   getBooks,
-  getBooksByTitle: {
-    type: new GraphQLList(BookType),
-    args: {
-      title: { type: GraphQLString },
-      page: { type: GraphQLInt },
-      limit: { type: GraphQLInt },
-    },
-    async resolve(parent: any, args: any) {
-      const { title, limit, page } = args;
-      return await prisma.book.findMany({
-        where: { title: { contains: title } },
-        take: limit,
-        skip: page * limit,
-        orderBy: {
-          title: "asc",
-        },
-      });
-    },
-  },
+  getBooksByTitle,
   getABookById: {
     type: BookType,
     args: { id: { type: GraphQLInt } },
