d8aced11dcf96cb1928f31e0a4ae32e8d1c712d4
Karanvir <karanvirboparai123@gmail.com>
2022-12-19 09:18:50 -0500

    created paginated books use case

---
+++ src/entity/book/use_cases/getPaginatedBooks.ts
@@ -0,0 +1,19 @@
+import { getBooks } from "../db/book-db";
+
+export default function makePaginatedBooks({
+  getPaginatedBooks,
+}: {
+  getPaginatedBooks: getBooks;
+}) {
+  return async function paginatedBooks({
+    limit = 10,
+    page,
+  }: {
+    limit: number;
+    page: number;
+  }) {
+    if (Number.isNaN(limit)) throw Error("Limit needs to be a number.");
+    if (Number.isNaN(page)) throw Error("Page needs to be a number.");
+    return await getPaginatedBooks({ limit, page });
+  };
+}
