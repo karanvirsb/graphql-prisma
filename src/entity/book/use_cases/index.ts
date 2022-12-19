7c928c3fa05b0198cc252e80b54897bb09c9116c
Karanvir <karanvirboparai123@gmail.com>
2022-12-19 10:18:17 -0500

    added get a book by id use case

--- src/entity/book/use_cases/index.ts
+++ src/entity/book/use_cases/index.ts
@@ -1,4 +1,5 @@
 import BookDb from "../db";
+import makeGetABookById from "./getABookById";
 import makeGetBooksByTitle from "./getBooksByTitle";
 import makePaginatedBooks from "./getPaginatedBooks";
 
@@ -10,15 +11,21 @@ const getBooksByTitleUseCase = makeGetBooksByTitle({
   getBooksByTitle: BookDb.getBooksByTitle,
 });
 
+const getABookByIdUseCase = makeGetABookById({
+  getABookById: BookDb.getABookById,
+});
+
 // types for use cases
 export type getPaginatedBooksType = typeof getPaginatedBooks;
 export type getBooksByTitleType = typeof getBooksByTitleUseCase;
+export type getABookByIdUseCaseType = typeof getABookByIdUseCase;
 
 const bookServices = Object.freeze({
   getPaginatedBooks,
   getBooksByTitleUseCase,
+  getABookByIdUseCase,
 });
 
 export default bookServices;
 
-export { getPaginatedBooks, getBooksByTitleUseCase };
+export { getPaginatedBooks, getBooksByTitleUseCase, getABookByIdUseCase };
