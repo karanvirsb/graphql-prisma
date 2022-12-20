c8dc13a887ab3110b3c8d33155a1aabedaa295a0
Karanvir <karanvirboparai123@gmail.com>
2022-12-20 09:47:40 -0500

    created jest config

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["./node_modules/**"],
+};
+
+export default config;
