2a5ac628e520941884da9d57606e12b7ee44ca0d
Karanvir <karanvirboparai123@gmail.com>
2022-12-20 10:34:43 -0500

    created mock data for book

---
+++ src/__test__/entity/book.ts
@@ -0,0 +1,37 @@
+import casual from "casual";
+
+export function mockBook({
+  id,
+  author_id,
+  library_id,
+  publisher_id,
+}: {
+  id: number;
+  author_id: number;
+  library_id: number;
+  publisher_id: number;
+}) {
+  return {
+    id,
+    asin: casual.integer(100000, 1000000) + casual.letter,
+    ISBN10: casual.integer(100000, 1000000) + casual.letter,
+    author_id: 1,
+    currency: "USD",
+    description: casual.catch_phrase,
+    final_price: "$" + casual.integer,
+    format: JSON.stringify([
+      { name: casual.name, price: "$" + casual.integer(), url: casual.url },
+    ]),
+    image_url: casual.url,
+    images_count: casual.integer(),
+    item_weight: casual.name,
+    product_dimensions: casual.name,
+    rating: casual.name,
+    reviews_count: casual.random,
+    publisher_id: 1,
+    title: casual.title,
+    url: casual.url,
+    categories: [...new Array(3)].map((i) => casual.name),
+    library_id: 1,
+  };
+}
