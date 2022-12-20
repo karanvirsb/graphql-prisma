2e9ab0497aa36dfe03d1c850ea1e04ade3288b40
Karanvir <karanvirboparai123@gmail.com>
2022-12-20 10:19:27 -0500

    changed prisma url

--- src/__test__/prisma/singleton.ts
+++ src/__test__/prisma/singleton.ts
@@ -3,7 +3,7 @@ import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended";
 
 import prisma from "../../../prisma";
 
-jest.mock("./client", () => ({
+jest.mock("../../../prisma", () => ({
   __esModule: true,
   default: mockDeep<PrismaClient>(),
 }));
