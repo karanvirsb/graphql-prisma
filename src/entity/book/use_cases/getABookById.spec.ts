26d8dd97bd6a019ca94eba9c6170f87028d0b3e4
Karanvir <karanvirboparai123@gmail.com>
2022-12-20 11:20:50 -0500

    renamed and created mock test

---
+++ src/entity/book/use_cases/getABookById.spec.ts
@@ -0,0 +1,26 @@
+import makeGetABookById from "./getABookById";
+import { prismaMock } from "../../../__test__/prisma/singleton";
+import makeBookDb from "../db/book-db";
+import { mockBook } from "../../../__test__/entity/book";
+
+describe("Getting a book by id", () => {
+  const BookDb = makeBookDb({ db: prismaMock.book });
+  const getABookById = makeGetABookById({ getABookById: BookDb.getABookById });
+
+  test("SUCCESS: Book found by id.", async () => {
+    const book = mockBook({
+      id: 4,
+      author_id: 1,
+      library_id: 2,
+      publisher_id: 3,
+    });
+
+    // const addedBook = await prismaMock.book.create({ data: book });
+    prismaMock.book.findFirst.mockResolvedValue(book);
+
+    // const foundBook = await prismaMock.book.findMany();
+    const foundBook = await getABookById({ id: book.id });
+
+    expect(foundBook?.id).toBe(book.id);
+  });
+});
