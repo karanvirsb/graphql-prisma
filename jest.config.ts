7cba24c4fba2be5255d722848a67e9ea40286e15
Karanvir <karanvirboparai123@gmail.com>
2022-12-20 10:18:54 -0500

    added skip for node modules

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ import type { Config } from "jest";
 const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["./node_modules/**"],
+  testPathIgnorePatterns: ["node_modules"],
 };
 
 export default config;
