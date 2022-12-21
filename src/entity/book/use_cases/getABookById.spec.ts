2384abff18ffa6ac639e5af332d3637c4acfa659
Karanvir <karanvirboparai123@gmail.com>
2022-12-21 09:05:44 -0500

    did a test for not getting the correct result

--- src/entity/book/use_cases/getABookById.spec.ts
+++ src/entity/book/use_cases/getABookById.spec.ts
@@ -23,4 +23,21 @@ describe("Getting a book by id", () => {
 
     expect(foundBook?.id).toBe(book.id);
   });
+
+  test("ERROR: Book does not exist.", async () => {
+    const book = mockBook({
+      id: 4,
+      author_id: 1,
+      library_id: 2,
+      publisher_id: 3,
+    });
+
+    // const addedBook = await prismaMock.book.create({ data: book });
+    prismaMock.book.findFirst.mockResolvedValue(book);
+
+    // const foundBook = await prismaMock.book.findMany();
+    const foundBook = await getABookById({ id: 0 });
+
+    expect(foundBook?.id).not.toBe(0);
+  });
 });
